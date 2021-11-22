ec31209468be34a53921ab94686480a86a2d7d7b
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-22 16:19:19 +0900

    added basic seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting everything...:0"
+User.destroy_all
+Artist.destroy_all
+Performance.destroy_all
+Tip.destroy_all
+
+puts "Creating users..."
+
+emanuel = User.create!(name:"Emanuel", email: "emanuel@lewagon.com", )
+gareth = User.create!(name:"Gareth", email: "gareth@lewagon.com")
+anju = User.create!(name:"Anju", email: "anju@lewagon.com")
+louis = User.create!(name:"Louis", email: "louis@lewagon.com")
+
+
+puts "Creating artists..."
+
+cups = Artist.new(name: "Cups", user: emanuel)
+mighty = Artist.new(name: "Mighty Dumpling", user:gareth)
+trashcan = Artist.new(name: "Trashcan Paradise", user: anju)
+
+puts "Creating performances..."
+
+trash_perf = Performance.create!(name:"Trashcans live on the streets", artist: trashcan)
+mighty_perf = Performance.create!(name:"Mightys live on the streets", artist: mighty)
+cups_perf = Performance.create!(name:"Cups live on the streets", artist: cups)
+
+puts "Creating tips..."
+
+Tip.create!(amount:300, user:louis, performance:cups_perf)
+Tip.create!(amount:1000, user:gareth, performance:mighty_perf)
+Tip.create!(amount:100, user:anju, performance:trash_perf)
+
+puts "created #{User.count} users, #{Artist.count} artists, #{Performance.count} performances, and #{Tip.count} tips!"
