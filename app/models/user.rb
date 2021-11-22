5db33792f4676f9b47a0b8fd51f1cf90afa0f157
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-22 16:19:56 +0900

    deleted name validation on user because there is not name on user. only email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
 end
