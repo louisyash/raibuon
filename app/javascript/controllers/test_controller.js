2ffc694e1fad836ddc0565d5912233c58189b7db
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-24 22:36:32 +0900

    copied doug code

--- app/javascript/controllers/test_controller.js
+++ app/javascript/controllers/test_controller.js
@@ -1,34 +1,15 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['items', 'input', "container"];
+  static targets = ['form', 'input', "list"];
 
   update() {
-    const url = `performances?query=${this.inputTarget.value}`
-    console.log(this.inputTarget.action)
+    console.log(this.formTarget)
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
-        this.containerTarget.outerHTML = data;
+        this.listTarget.outerHTML = data
       })
   }
-  // send(event) {
-  //   let cards = this.itemsTargets
-  //   let query = this.inputTarget.value;
-  //   let container = this.containerTarget
-  //   event.preventDefault();
-
-  //   const showCards = () => {
-  //     cards.forEach((card) => {
-  //       if (card.innerText.includes(query)); {
-  //         container.innerHTML = "";
-  //         container.insertAdjacentHTML("beforeend", card);
-  //         console.log(query)
-  //       }
-  //     });
-  //   }
-  //   if (event) {
-  //     showCards()
-  //     }
-  // }
 }
