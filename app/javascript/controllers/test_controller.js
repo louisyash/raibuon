335ee27c2e5ff911a79f698a44ab94b4e12a3323
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-24 15:34:46 +0900

    made the date show on performances index

--- app/javascript/controllers/test_controller.js
+++ app/javascript/controllers/test_controller.js
@@ -4,16 +4,22 @@ export default class extends Controller {
   static targets = ['items', 'input', "container"];
 
   send(event) {
-
+    let cards = this.itemsTargets
+    let query = this.inputTarget.value;
+    let container = this.containerTarget
     event.preventDefault();
-if (event) {
-    this.itemsTargets.forEach((card) => {
-      let query = this.inputTarget.value;
-      if (card.innerText.includes(query)) {
-        this.containerTarget.innerHTML = "";
-        this.containerTarget.insertAdjacentElement("beforeend", card);
+
+    const showCards = () => {
+      cards.forEach((card) => {
+        if (card.innerText.includes(query)); {
+          // container.innerHTML = "";
+          container.insertAdjacentHTML("beforeend", "<p>Hello</p>");
+          console.log(query)
+        }
+      });
+    }
+    if (event) {
+      showCards()
       }
-    });
-  }
   }
 }
