cd1c8dc2157349c68974fca5b5320fbc45170add
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-24 23:07:24 +0900

    got it to partially work. need to finalize

--- app/javascript/controllers/test_controller.js
+++ app/javascript/controllers/test_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   static targets = ['form', 'input', "list"];
 
   update() {
-    console.log(this.formTarget)
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
-        this.listTarget.outerHTML = data
+        // this.listTarget.innerHTML = data
+        console.log()
       })
   }
 }
