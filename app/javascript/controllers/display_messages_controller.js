b636e9a5e67f5faaaed45745aac774a243aa90b0
emanuel caro <darkemlord@gmail.com>
2021-11-24 17:12:35 +0900

    view more and view less html maked

--- app/javascript/controllers/display_messages_controller.js
+++ app/javascript/controllers/display_messages_controller.js
@@ -3,10 +3,16 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ['display', 'box'];
   connect(){
-    console.log(this.boxTarget)
+    // console.log(this.boxTarget)
+    // console.log(this.displayTarget)
   }
 
   display(){
     this.boxTarget.classList.toggle('messages-content')
+    if (this.displayTarget.innerHTML === 'View 4 more messages'){
+      this.displayTarget.innerHTML = 'Display less';
+    }else{
+      this.displayTarget.innerHTML = 'View 4 more messages';
+    }
   }
 }
