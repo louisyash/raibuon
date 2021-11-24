6d1834df4aee77a1e36605f61c1365f6e74b727a
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-24 17:49:33 +0900

    deleted test div

--- app/javascript/controllers/test_controller.js
+++ app/javascript/controllers/test_controller.js
@@ -3,14 +3,14 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ['items', 'input', "container"];
 
-  // update() {
-  //   const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-  //   fetch(url, { headers: { 'Accept': 'text/plain' } })
-  //     .then(response => response.text())
-  //     .then((data) => {
-  //       this.listTarget.outerHTML = data;
-  //     })
-  // }
+  update() {
+    const url = `${this.inputTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.containerTarget.outerHTML = data;
+      })
+  }
   // send(event) {
   //   let cards = this.itemsTargets
   //   let query = this.inputTarget.value;
