21a3b7fed3bb946fcdb08772877ae64600e87d59
emanuel caro <darkemlord@gmail.com>
2021-11-24 17:35:10 +0900

    new style for performance form

--- app/javascript/controllers/display_messages_controller.js
+++ app/javascript/controllers/display_messages_controller.js
@@ -2,15 +2,11 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = ['display', 'box'];
-  connect(){
-    // console.log(this.boxTarget)
-    // console.log(this.displayTarget)
-  }
 
   display(){
     this.boxTarget.classList.toggle('messages-content')
     if (this.displayTarget.innerHTML === 'View 4 more messages'){
-      this.displayTarget.innerHTML = 'Display less';
+      this.displayTarget.innerHTML = 'View less';
     }else{
       this.displayTarget.innerHTML = 'View 4 more messages';
     }
