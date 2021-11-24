635ce2ffcedcd7b0e496d8b01e2ad0d6488eb3f9
Anju <88181896+ansanigom@users.noreply.github.com>
2021-11-24 16:44:58 +0900

    routes changed for tips

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :performances, only: %i[index show new create] do
-    resources :tips, only: %i[new create]
+    resources :tips, only: %i[new create show]
   end
 
   resources :artists, only: %i[new create]
