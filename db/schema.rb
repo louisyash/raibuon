719da1d2c1a7a5018169611588fd15c8860fdc71
Anju <88181896+ansanigom@users.noreply.github.com>
2021-11-24 18:01:23 +0900

    added column to tips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_071204) do
+ActiveRecord::Schema.define(version: 2021_11_24_085657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_071204) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "amount_cents", default: 0, null: false
+    t.string "checkout_session_id"
     t.index ["performance_id"], name: "index_tips_on_performance_id"
     t.index ["user_id"], name: "index_tips_on_user_id"
   end
