c60218bded6f45b0443be99de4b4d3bcf7f51cb4
Anju <88181896+ansanigom@users.noreply.github.com>
2021-11-25 11:00:05 +0900

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :performances, only: %i[index show new create] do
     resources :tips, only: %i[new create show] do
