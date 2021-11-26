dfca61e8165f87fac2fbb3774252322e705f4de6
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-26 14:17:48 +0900

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_071110) do
+ActiveRecord::Schema.define(version: 2021_11_26_041431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
