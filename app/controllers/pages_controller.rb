97a74d98a10640e21572386b9d2ee173d66edbd1
gareth perilli <garethperilli@gmail.com>
2021-11-29 22:39:47 +0900

    backgroud banner image position center

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, except: [ :home, :index]
 
   def home
+    @performances = policy_scope(Performance).order(created_at: :desc)
+    authorize @performances
+
   end
 end
