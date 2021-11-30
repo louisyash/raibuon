af43fb65f72089d26bf73af6507468a688304d9c
gareth perilli <garethperilli@gmail.com>
2021-11-30 11:49:07 +0900

    functioning info windows

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,11 @@ class PagesController < ApplicationController
   def home
     @performances = policy_scope(Performance).order(created_at: :desc)
 
-
     @markers = @performances.geocoded.map do |performance|
       {
         lat: performance.latitude,
-        lng: performance.longitude
+        lng: performance.longitude,
+        info_window: render_to_string(partial: "shared/info_window", locals: { performance: performance })
       }
     end
     authorize @performances
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,8 +12,10 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
       .addTo(map);
   });
   fitMapToMarkers(map, markers);
