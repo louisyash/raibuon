901112d3e4329ae915def3383e1accef06aed3a6
Louis Yasuhiro <lyasuhir@gmail.com>
2021-12-02 14:16:50 +0900

    added new logic to js

--- app/javascript/controllers/rankings_controller.js
+++ app/javascript/controllers/rankings_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
     const artists = this.artistTargets
     artists.forEach((element) => {
       if (element.getAttribute("data-index") == index) {
-        element.classList.add("d-none")
+        element.classList.toggle("d-none")
       } else if (element.classList.contains("d-none")) {
         element.classList.remove("d-none")
       }
@@ -17,4 +17,14 @@ export default class extends Controller {
     console.log(index)
     console.log(artists)
  }
+
+ reveal(event){
+   console.log(event.currentTarget)
+   const index = event.currentTarget.getAttribute("data-index")
+   const artists = this.artistTargets
+   console.log(artists)
+   artists.forEach((element) => {
+     element.classList.toggle("d-none")
+     }
+   )}
 }
