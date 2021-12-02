afb449fe5d3e7a70a20dc23aa06b86cd0fd6c74f
Louis Yasuhiro <lyasuhir@gmail.com>
2021-12-02 15:32:51 +0900

    tip tanking hide completed

--- app/javascript/controllers/rankings_controller.js
+++ app/javascript/controllers/rankings_controller.js
@@ -4,25 +4,9 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ['artist'];
 
-//   showRankings(event) {
-//     const index = event.currentTarget.getAttribute("data-index")
-//     const artists = this.artistTargets
-//     artists.forEach((element) => {
-//       if (element.getAttribute("data-index") == index) {
-//         element.classList.toggle("d-none")
-//       } else if (element.classList.contains("d-none")) {
-//         element.classList.remove("d-none")
-//       }
-//     })
-//     console.log(index)
-//     console.log(artists)
-//  }
-
  reveal(event){
-   console.log(event.currentTarget)
    const index = event.currentTarget.getAttribute("data-index")
    const artists = this.artistTargets
-   console.log(artists)
    artists.forEach((element) => {
        element.classList.toggle("d-none")
      }
